9bc02b2cdb4159d9c45900eaccc7621243c0b1f0
sniyaz <sniyaz@bk.ru>
2023-05-13 19:59:18 +0300

    Home Work Android 3.3 or 10 savedInFileJson

--- app/src/main/java/ru/netology/nMediaApp/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/nMediaApp/repository/PostRepositoryInMemoryImpl.kt
@@ -10,88 +10,90 @@ import ru.netology.nMediaApp.dto.Post
 import java.lang.reflect.Type
 
 class PostRepositoryInMemoryImpl(
-    context: Context
+    val context: Context
 ) : PostRepository {
 
-    companion object{
-        private const val POST_KEY = "POST_KEY"
+    companion object {
+        private const val FILE_NAME = "posts.json"
     }
 
-    private var nextId = 1L
-    private var posts: List<Post> = emptyList()
-        set(value){
-            field = value
-            sync()
-        }
-    val  prefs = context.getSharedPreferences("posts", Context.MODE_PRIVATE)
+    //private var nextId = 1L
     private val gson = Gson()
     private val type = TypeToken.getParameterized(List::class.java, Post::class.java).type
 
+    private var posts: List<Post> = readPosts()
+        set(value) {
+            field = value
+            sync()
+        }
+
     private val data = MutableLiveData(posts)
 
-    init {
-        readPosts()
-    }
 
     override fun getAll(): LiveData<List<Post>> = data
 
     private fun sync() {
-        prefs.edit {
-            putString(POST_KEY, gson.toJson(posts))
+        context.openFileOutput(FILE_NAME, Context.MODE_PRIVATE).bufferedWriter().use {
+            it.write(gson.toJson(posts))
         }
     }
 
-    private fun readPosts(){
-        posts = prefs.getString(POST_KEY, null)?.let {
-            gson.fromJson<List<Post>>(it, type)
-        }.orEmpty()
-        data.value = posts
-    }
-    override fun likeById(id: Long) {
-        posts = posts.map {
-            if (it.id != id) {
-                it
-            } else {
-                var count = if (!it.likedByMe) it.copy(likes = it.likes + 1)
-                else it.copy(likes = it.likes - 1)
-                it.copy(likedByMe = !it.likedByMe, likes = count.likes)
-            }
+    private fun readPosts(): List<Post> {
+        val file = context.filesDir.resolve(FILE_NAME)
+        return if (file.exists()){
+        context.openFileInput(FILE_NAME).bufferedReader().use {
+            gson.fromJson(it, type)
         }
-        data.value = posts
+    } else{
+        emptyList()
     }
+}
 
-    override fun shareById(id: Long) {
-        posts = posts.map {
-            if (it.id != id) it
-            else it.copy(share = it.share + 1, shareByMe = it.shareByMe)
+override fun likeById(id: Long) {
+    posts = posts.map {
+        if (it.id != id) {
+            it
+        } else {
+            var count = if (!it.likedByMe) it.copy(likes = it.likes + 1)
+            else it.copy(likes = it.likes - 1)
+            it.copy(likedByMe = !it.likedByMe, likes = count.likes)
         }
-        data.value = posts
     }
+    data.value = posts
+}
 
-    override fun removeById(id: Long) {
-        posts = posts.filter { it.id != id }
-        data.value = posts
+override fun shareById(id: Long) {
+    posts = posts.map {
+        if (it.id != id) it
+        else it.copy(share = it.share + 1, shareByMe = it.shareByMe)
     }
+    data.value = posts
+}
 
-    override fun save(post: Post) {
-        if (post.id == 0L) {
-            posts = listOf(
-                post.copy(
-                    id = nextId++,
-                    autor = "Me",
-                    published = "Now",
-                    likedByMe = false,
-                    shareByMe = false,
-                    likes = 0,
-                    share = 0
-                )
-            ) + posts
-            data.value = posts
-            return
-        }
-        posts = posts.map {
-            if (it.id != post.id) it else it.copy(content = post.content)
-        }
+override fun removeById(id: Long) {
+    posts = posts.filter { it.id != id }
+    data.value = posts
+}
+
+override fun save(post: Post) {
+    if (post.id == 0L) {
+        posts = listOf(
+            post.copy(
+                id = (posts.firstOrNull()?.id ?: 0L) + 1,
+                autor = "Me",
+                published = "Now",
+                likedByMe = false,
+                shareByMe = false,
+                likes = 0,
+                share = 0
+            )
+        ) + posts
         data.value = posts
+        return
     }
+    posts = posts.map {
+        if (it.id != post.id) it else it.copy(content = post.content)
+    }
+    data.value = posts
+}
 }
